f52b18bd2684f33bbfb85270211bb4163f3a4b34
Rufin Hounkpe <rhounkpe@gmail.com>
2020-01-15 19:31:53 +0100

    Initial commit: setting datasource

---
+++ src/test/java/be/digicom/eventsmanager/EventsmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package be.digicom.eventsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
